281cb2baf7182b1b7e043305db7b0d0b83517e6f
mooz <stillpedant@gmail.com>
2010-07-10 10:26:10 +0900

    version 0.0.6

--- src/content/gpum.js
+++ src/content/gpum.js
@@ -64,7 +64,7 @@
      function safenBrowser(iframe) {
          let { docShell, contentWindow } = iframe;
 
-         contentWindow.print = function () {};
+         contentWindow.print = contentWindow.wrappedJSObject.print = function () {};
          docShell.allowJavascript = docShell.allowPlugins = docShell.allowSubframes = false;
      }
 
