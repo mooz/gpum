1df891adf06e1b3b2092d85aa93fc799df5d390a
Masafumi Oyamada <stillpedant@gmail.com>
2015-09-27 21:58:38 +0900

    Don't use chrome.jar

--- src/createpackage.sh
+++ src/createpackage.sh
@@ -3,23 +3,13 @@
 setopt extended_glob
 setopt nonomatch
 
-## ================================ ##
-
-# create jar file
-[[ -d chrome ]] || mkdir chrome
-
-rm -f chrome/gpum.jar
-zip -r -0 chrome/gpum.jar \
-    content/**/*.{js,xul,xml,css,png,gif}~(*~|.svn/*) \
-    locale/**/*~(*~|.svn/*) \
-    skin/**/*.*~(*~|.svn/*)
-
 # create xpi file
 rm -f gpum.xpi
 zip -r -9 gpum.xpi \
-    chrome/gpum.jar \
+    chrome.manifest \
+    content/**/*.{js,xul,xml,css,png,gif}~(*~|.svn/*) \
+    locale/**/*~(*~|.svn/*) \
+    skin/**/*.*~(*~|.svn/*) \
     defaults/**/*.*~(*~|.svn/*) \
     install.rdf \
     modules/*.jsm~(*~|.svn/*)
-cp chrome.manifest.pack /tmp/chrome.manifest
-zip -j -9 gpum.xpi /tmp/chrome.manifest
