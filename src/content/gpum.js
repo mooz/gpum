mooz/gpum

commit 8b1994a26075d47c896adea4865f9feea28fa337

Font size in panel/notification can be costomizable

src/content/gpum.js
@@ -387,7 +387,7 @@
                 case summary:
                     {
                         let url = gmail.getThreadBodyURL(id);
-                        let popup  = $('gpum-popup4preview');
+                        let previewWindow  = $('gpum-popup4preview');
                         let iframe = $('gpum-popup4preview-frame');
                         let title  = $('gpum-popup4preview-header-title');
 
@@ -416,7 +416,7 @@
                             destroy();
                         }
 
-                        popup.openPopup(popupOrigin, "bottomcenter topright");
+                        previewWindow.openPopup(popupOrigin, "bottomcenter topright");
                     }
 
                     break;
@@ -454,6 +454,22 @@
             get nowChecking() { return this._nowChecking; },
 
             get toolbarButtonInstalled() { return !!getToolbarButton(); },
+
+            get panelFontSize() {
+                return util.getIntPref(util.getPrefKey("panelFontSize")) + "px";
+            },
+
+            get notificationFontSize() {
+                return util.getIntPref(util.getPrefKey("notificationFontSize")) + "px";
+            },
+            openPopup:
+            function openPopup(origin) {
+                util.registerGlobalStyle("popup",
+                                         `
+#gpum-popup label { font-size : ${gpum.panelFontSize} !important; }
+                                         `);
+                popup.openPopup(origin, "bottomcenter topright");
+            },
 
             installToolbarButton:
             function installToolbarButton() {
@@ -509,6 +525,10 @@
 
             showNotification:
             function showNotification(title, message, onClick) {
+                util.registerGlobalStyle("notification",
+                                         `
+#notification-title, #notification-message-container description { font-size: ${gpum.notificationFontSize} !important; }
+                                         `);
                 util.showPopup(title, message, {
                     icon: "chrome://gpum/skin/icon64/gmail64.png",
                     clickable: typeof onClick === "function",
@@ -639,7 +659,7 @@
 
                     inboxLabel.textContent = gmail.xml.select("title").text.replace(/^Gmail - /, "");
 
-                    popup.openPopup(ev.originalTarget, "bottomcenter topright");
+                    gpum.openPopup(ev.originalTarget);
                 }
             },
 
@@ -692,7 +712,7 @@
                     document.documentElement.removeChild(popup);
                 }, false);
 
-                popup.openPopup(ev.originalTarget, "bottomcenter topright");
+                gpum.openPopup(ev.originalTarget);
             },
 
             login:

src/content/config.xul
@@ -16,6 +16,12 @@
 
     <prefpane id="prefpane-general" label="&config.general;" flex="1" onpaneload="">
         <preferences>
+            <preference id="extensions.gpum.notificationFontSize"
+                        name="extensions.gpum.notificationFontSize"
+                        type="int" />
+            <preference id="extensions.gpum.panelFontSize"
+                        name="extensions.gpum.panelFontSize"
+                        type="int" />
             <preference id="extensions.gpum.updateInterval"
                         name="extensions.gpum.updateInterval"
                         type="int" />
@@ -59,6 +65,30 @@
                         name="extensions.gpum.markAsReadOnArchive"
                         type="bool" />
         </preferences>
+
+        <groupbox orient="vertical">
+          <caption label="&config.appearance;" />
+          <vbox flex="1">
+            <hbox align="center">
+              <label >&config.panelFontSize;: </label>
+              <textbox id="config.panelFontSize"
+                       type="number"
+                       preference="extensions.gpum.panelFontSize"
+                       min="8"
+                       max="100" />
+            </hbox>
+
+            <hbox align="center">
+              <label >&config.notificationFontSize;: </label>
+              <textbox id="config.notificationFontSize"
+                       type="number"
+                       preference="extensions.gpum.notificationFontSize"
+                       min="8"
+                       max="100" />
+            </hbox>
+          </vbox>
+        </groupbox>
+
         <groupbox orient="vertical">
             <caption label="&config.general;" />
             <vbox flex="1">

src/content/notification/notification.css
@@ -35,13 +35,13 @@ window {
 }
 
 #notification-title {
-    font-size   : x-small;
+    /* font-size   : small; */
     font-weight : bold;
     padding     : 0 4px;
 }
 
 #notification-message-container description {
-    font-size  : x-small;
+    /* font-size  : small; */
 }
 
 /* ============================================================

src/defaults/preferences/gmml-prefs.js
@@ -11,3 +11,5 @@ pref("extensions.gpum.notificationDisplayDuration", 8);
 pref("extensions.gpum.notificationCropCharacterCount", 30);
 pref("extensions.gpum.shouldCropLabel", true);
 
+pref("extensions.gpum.panelFontSize", 15);
+pref("extensions.gpum.notificationFontSize", 15);

src/locale/en-US/config.dtd
@@ -1,5 +1,9 @@
 <!ENTITY config.title "GPUM settings">
 
+<!ENTITY config.appearance "Appearance">
+<!ENTITY config.notificationFontSize "Notification font size (px)">
+<!ENTITY config.panelFontSize "Panel font size (px)">
+
 <!ENTITY config.general "General">
 <!ENTITY config.about "About">
 

src/locale/ja/config.dtd
@@ -1,5 +1,8 @@
 <!ENTITY config.title "GPUM の設定">
 
+<!ENTITY config.appearance "見た目">
+<!ENTITY config.notificationFontSize "ポップアップの文字サイズ (px)">
+<!ENTITY config.panelFontSize "パネルの文字サイズ (px)">
 <!ENTITY config.general "一般">
 <!ENTITY config.about "情報">
 

src/modules/util.jsm
@@ -413,6 +413,33 @@ const util = {
         src = src.replace(/^<\?xml\s+version\s*=\s*(?:"[^"]+"|'[^']+')[^?]*\?>/, "");
         var domParser = util.createDOMParser();
         return domParser.parseFromString(src, "text/xml");
+    },
+
+    // ============================================================ //
+    // Style
+    // ============================================================ //
+
+    registeredStyles: {},
+    registerGlobalStyle:
+    function registerGlobalStyle(name, style) {
+        let styleService = Cc["@mozilla.org/content/style-sheet-service;1"]
+                .getService(Ci.nsIStyleSheetService);
+        if (this.registeredStyles.hasOwnProperty(name)) {
+            // Unregister if exists
+            styleService.unregisterSheet(this.registeredStyles[name], styleService.USER_SHEET);
+            delete this.registeredStyles[name];
+        }
+        // Register style with name
+        this.registeredStyles[name] = this.constructStyleDataURI(style);
+        styleService.loadAndRegisterSheet(this.registeredStyles[name], styleService.USER_SHEET);
+    },
+
+    constructStyleDataURI:
+    function constructStyleDataURI(style) {
+        var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+        var namespace = "@namespace url('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul');";
+        return ios.newURI("data:text/css," + encodeURIComponent(namespace + style),
+                          null, null);
     }
 };
 

src/skin/classic/gpum/css/popup.css
@@ -68,7 +68,7 @@
 }
 
 #gpum-popup label {
-    font-size : 12px !important;
+    /* font-size : 14px !important; */
     /* text-decoration : underline !important; */
 }
 

commit 26ac41435375f3972c47b468767a1c76a5b22ec4

Fix login menu (regression in 8b1994a)

src/content/gpum.js
@@ -79,7 +79,7 @@
     window.addEventListener("load", function () {
         window.removeEventListener("load", arguments.callee, false);
 
-        let popup          = $("gpum-popup");
+        let mailListPopup  = $("gpum-popup");
         let statusbarIcon  = $("gpum-statusbar-icon");
         let statusbarCount = $("gpum-statusbar-count");
 
@@ -107,7 +107,7 @@
         document.addEventListener(gmail.UPDATE_EVENT, handleUpdate, false);
 
         let unreadContainer = $E("vbox", { flex : 1 });
-        popup.appendChild(unreadContainer);
+        mailListPopup.appendChild(unreadContainer);
 
         // ============================================================ //
 
@@ -269,7 +269,7 @@
                 url = url.replace(/^http:\/\/mail\.google\.com\//, "https://mail.google.com/");
             util.visitLink(url);
             if (!cont)
-                popup.hidePopup();
+                mailListPopup.hidePopup();
         }
 
         function appendEntry(scrollBox, unread) {
@@ -462,13 +462,13 @@
             get notificationFontSize() {
                 return util.getIntPref(util.getPrefKey("notificationFontSize")) + "px";
             },
-            openPopup:
-            function openPopup(origin) {
+            openMailListPopup:
+            function openMailListPopup(origin) {
                 util.registerGlobalStyle("popup",
                                          `
 #gpum-popup label { font-size : ${gpum.panelFontSize} !important; }
                                          `);
-                popup.openPopup(origin, "bottomcenter topright");
+                mailListPopup.openPopup(origin, "bottomcenter topright");
             },
 
             installToolbarButton:
@@ -659,7 +659,7 @@
 
                     inboxLabel.textContent = gmail.xml.select("title").text.replace(/^Gmail - /, "");
 
-                    gpum.openPopup(ev.originalTarget);
+                    gpum.openMailListPopup(ev.originalTarget);
                 }
             },
 
@@ -672,7 +672,7 @@
                     }
                 });
 
-                let popup = $E("menupopup");
+                let loginMenuPopup = $E("menupopup");
 
                 let shouldInsertSeparator = false;
                 for (let [username, password] in Iterator(collectedLogins)) {
@@ -682,19 +682,19 @@
                         value : password
                     });
 
-                    popup.appendChild(menuItem);
+                    loginMenuPopup.appendChild(menuItem);
                 }
                 if (shouldInsertSeparator)
-                    popup.appendChild($E("menuseparator"));
+                    loginMenuPopup.appendChild($E("menuseparator"));
 
-                popup.appendChild($E("menuitem", {
+                loginMenuPopup.appendChild($E("menuitem", {
                     label : util.getLocaleString("openLoginPage")
                 }));
 
-                document.documentElement.appendChild(popup);
+                document.documentElement.appendChild(loginMenuPopup);
 
-                popup.addEventListener("command", function (ev) {
-                    popup.removeEventListener("command", arguments.callee, false);
+                loginMenuPopup.addEventListener("command", function (ev) {
+                    loginMenuPopup.removeEventListener("command", arguments.callee, false);
 
                     let elem  = ev.target;
 
@@ -707,12 +707,12 @@
                         gmail.openLoginPage();
                 }, false);
 
-                popup.addEventListener("popuphidden", function (ev) {
-                    popup.removeEventListener("popuphidden", arguments.callee, false);
-                    document.documentElement.removeChild(popup);
+                loginMenuPopup.addEventListener("popuphidden", function (ev) {
+                    loginMenuPopup.removeEventListener("popuphidden", arguments.callee, false);
+                    document.documentElement.removeChild(loginMenuPopup);
                 }, false);
 
-                gpum.openPopup(ev.originalTarget);
+                loginMenuPopup.openPopup(ev.originalTarget);
             },
 
             login: